2b79cd7708ddc97023c54ec91c0f4c02a6e54e93
uditagarwal29 <57745285+uditagarwal29@users.noreply.github.com>
2022-02-01 16:26:32 +0530

    Update merge2Sorted.java

--- Linked List/merge2Sorted.java
+++ Linked List/merge2Sorted.java
@@ -1,5 +1,20 @@
-public class merge2Sorted {
-    public static void main(String[] args) {
-        
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null) {
+            return list2;
+        }
+        if (list2 == null) {
+            return list1;
+        }
+        ListNode c;
+        if (list1.val > list2.val) {
+            c = list2;
+            c.next = mergeTwoLists(list1, list2.next);
+        } else {
+            c = list1;
+            c.next = mergeTwoLists(list1.next, list2);
+        }
+
+        return c;
     }
 }
